1386ce7aeb2188e4cc1b12527ef167bbac9de518
ksdevinney <katie@devinney.org>
2021-10-02 21:49:47 -0500

    header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,8 +4,10 @@ const Header = () => {
     return (
         <header>
             <h1>Stitchmark</h1>
+            <h3>Todos</h3>
+            <h3>Current projects</h3>
         </header>
     )
 }
 
-export default Header
+export default Header;
